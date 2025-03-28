e16b4360ff0f19e7ba33ff3035cf561d29be7fc4
TUFB760\sergi <sergiojonathancastillejo@gmail.com>
2025-03-27 23:28:56 -0600

    crear custom provider http axios

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -3,11 +3,13 @@ import { PokemonModule } from 'src/pokemon/pokemon.module';
 
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
+import { CommonModule } from 'src/common/common.module';
 @Module({
     controllers: [SeedController],
     providers: [SeedService],
     imports: [
-        PokemonModule
+        PokemonModule,
+        CommonModule
     ]
 })
 export class SeedModule { }
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,28 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import axios, { AxiosAdapter, AxiosInstance } from 'axios';
 import { Model } from 'mongoose';
 import { PokeResponse } from './interfaces/poke-response.interface';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
-
-    // constructor(
-    //   @InjectModel( Pokemon.name )
-    //   private readonly pokemonModel: Model<Pokemon>,
-
-    //   private readonly http: AxiosAdapter,
-    // ) {}
-    private readonly axios: AxiosInstance = axios
+    // private readonly axios: AxiosInstance = axios
     constructor(
         @InjectModel(Pokemon.name)
         private readonly pokemonModel: Model<Pokemon>,
+        private readonly http: AxiosAdapter
     ) { }
 
     async executedSeed() {
         await this.pokemonModel.deleteMany({});
-        const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=500');
+        const data = await this.http.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=500');
         const pokemonToInsert: { name: string, no: number }[] = [];
         // data.results.forEach(async ({ name, url }) => {
         //     const segments = url.split('/');
