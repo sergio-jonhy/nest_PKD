1adbd094e558b449d3dec0c17f15d261011fcd69
TUFB760\sergi <sergiojonathancastillejo@gmail.com>
2025-03-28 00:40:46 -0600

    seccion 9: configuration loader

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,13 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { EnvConfiguration } from './config/env.config';
 @Module({
     imports: [
         
-        ConfigModule.forRoot(),
+        ConfigModule.forRoot({
+            load: [EnvConfiguration]
+        }),
 
         ServeStaticModule.forRoot({rootPath: join(__dirname,'..','public'),}),
         
